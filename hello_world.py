8c38a07ef22119c941467a70b80e5cffd5498136
Fernandez Coto <Luis.FernandezCoto@bsci.com>
2021-06-17 18:58:06 -0600

    Hello world

--- hello_world.py
+++ hello_world.py
@@ -1,1 +1,10 @@
-print("Hello Hacker Rank")
+# Read a full line of input from stdin and save it to our dynamically typed
+# variable, input_string.
+input_string = input()
+
+# Print a string literal saying "Hello, World." to stdout.
+print('Hello, World.')
+
+# TODO: Write a line of code here that prints the contents of input_string
+# to stdout.
+print(input_string)
