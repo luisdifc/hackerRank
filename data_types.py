49ad9581e085ef378acbb9f41efae5f1e10aa2d1
Fernandez Coto <Luis.FernandezCoto@bsci.com>
2021-06-25 15:52:23 -0600

    Data types challenge

---
+++ data_types.py
@@ -0,0 +1,10 @@
+i = 4
+d = 4.0
+s = 'HackerRank '
+new_int = int(input())
+new_double = float(input())
+new_string = str(input())
+
+print(f"{i + new_int}")
+print(f"{d + new_double}")
+print(f"{s + new_string}")
